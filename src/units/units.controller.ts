ab2e4b1421fc9865dad4cd848904e2f7c01fc47c
dev <dev@devui-MacBookPro.local>
2021-05-10 18:57:00 +0900

    controller option add

--- src/units/units.controller.ts
+++ src/units/units.controller.ts
@@ -1,15 +1,16 @@
-import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { unitDto } from './dto/unitDto';
 
 @Controller('units')
 export class UnitsController {
   @Get()
-  findAll(@Req() req: Request): string {
+  findAll(): string {
     return 'This action returns all units';
   }
 
   @Post('/create')
-  create(@Body() body: unitDto): string {
+  create(@Body() body: unitDto): unitDto {
+    console.log(body.name);
     return Object.assign({
       name: body.name,
       damage: body.damage,
